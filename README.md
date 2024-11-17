1e5a357a5b332a66711e9dadc5f3fd7c903c46b6
shahzadamodassir <shahzadamodassir@gmail.com>
2024-11-17 22:58:55 +0530

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,83 @@
 # PHP URLSearchParams
 
 A PHP implementation of URLSearchParams for handling query parameters easily.
+
+<p align="center">
+<a href="https://github.com/shahzadamodassir"><img src="https://img.shields.io/badge/Author-Shahzada%20Modassir-%2344cc11?style=flat-square"/></a>
+<a href="LICENSE"><img src="https://img.shields.io/github/license/lazervel/URLSearchParams?style=flat-square"/></a>
+<a href="https://packagist.org/packages/web/url-search-params"><img src="https://img.shields.io/packagist/dt/web/url-search-params.svg?style=flat-square" alt="Total Downloads"></a>
+<a href="https://github.com/lazervel/URLSearchParams/stargazers"><img src="https://img.shields.io/github/stars/lazervel/URLSearchParams?style=flat-square"/></a>
+<a href="https://github.com/lazervel/URLSearchParams/releases"><img src="https://img.shields.io/github/release/lazervel/URLSearchParams.svg?style=flat-square" alt="Latest Version"></a>
+</p>
+
+## Composer Installation
+
+Installation is super-easy via [Composer](https://getcomposer.org)
+
+```bash
+composer require web/url-search-params
+```
+
+or add it by hand to your `composer.json` file.
+
+## The Features of PHP URLSearchParams
+
+- [entries()](#entries)
+- [tupples()](#tupples)
+
+## Autoloading
+
+```php
+use Web\URLSearchParams\URLSearchParams;
+require 'vendor/autoload.php';
+```
+
+### Creating object without parameter
+```php
+$usp = new URLSearchParams();
+```
+
+### Creating object without '?' query string
+```php
+$usp = new URLSearchParams('name=foo&id=123&age=12');
+```
+
+### Creating object with '?' query string
+```php
+$usp = new URLSearchParams('?name=foo&id=123&age=12');
+```
+
+### Creating object query paire array with iterable [name, value]
+```php
+$usp = new URLSearchParams([['name', 'foo'], ['id', 123], ['age', 12]]);
+```
+
+### Creating object with associative array [key=>value] paire
+```php
+$usp = new URLSearchParams(['name'=> 'foo', 'id'=> 123, 'age'=> 12]);
+```
+
+### Creating object with object
+```php
+$usp = new URLSearchParams(stdClass Object
+(
+  [name] => foo
+  [id] => 123
+  [age] => 12
+));
+```
+
+## entries()
+
+```php
+$usp->entries();
+```
+
+## tupples()
+
+```php
+$usp->tupples();
+```
+
+## Resources
+- [Report issue](https://github.com/lazervel/URLSearchParams/issues) and [send Pull Request](https://github.com/lazervel/URLSearchParams/pulls) in the [main Lazervel repository](https://github.com/lazervel/URLSearchParams)
--- src/URLSearchParams.php
+++ src/URLSearchParams.php
@@ -19,13 +19,196 @@ declare(strict_types=1);
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+
 namespace Web\URLSearchParams;
 
 class URLSearchParams
 {
-  public function __construct()
+  /**
+   * $init instance
+   * 
+   * @var string|string[][]|array<string,string>|\Web\URLSearchParams\URLSearchParams
+   */
+  private $params;
+
+  /**
+   * Stored all real param data in tupples.
+   * 
+   * @var array<string,array|string>
+   */
+  private $tupples;
+
+  /**
+   * Stored modified param data in entries.
+   * 
+   * @var array<array<string,string>>
+   */
+  private $entries;
+
+  /**
+   * Creates a new URLSearchParams constructor
+   * Initializes a new instance of URLSearchParams with the given value.
+   * 
+   * @param string|string[][]|array<string,string>|\Web\URLSearchParams\URLSearchParams $init
+   * @return void
+   */
+  public function __construct($init = null)
   {
-    
+    $this->params = $init;
+    if ($init instanceof URLSearchParams) {
+      $this->tupples = $init->tupples();
+      $this->entries = $init->entries();
+    }
+  }
+
+  /**
+   * Returns an array of key, value pairs for every entry in the search params.
+   * 
+   * @return array<array<string,string>> All URLSearchParams entries.
+   */
+  public function entries() : array
+  {
+    return $this->entries ?? $this->makeEntries($this->params);
+  }
+
+  /**
+   * Check Assoc array, Returns true for Assoc, Otherwise false
+   * 
+   * @param array $array [required]
+   * @return bool Array assoc for true, Otherwise false
+   * 
+   * @example Example usage:
+   * isAssoc([1,2,3,4,5,9,8,7,6])           // Outputs: false
+   * isAssoc(['foo'=> 'bar', 'baz'=> true]) // Outputs: true
+   */
+  private function isAssoc(array $array) : bool
+  {
+    if (!\count($array)) return false;
+    return \range(0, \count($array) - 1) !== \array_keys($array);
+  }
+
+  /**
+   * Converts a given value to its string representation.
+   * 
+   * This method takes an input of any data type and returns its string representation.
+   * It can handle integers, floats, arrays, objects, etc.
+   * 
+   * @param mixed $value [required]
+   * @return string The string representation of the input value.
+   * 
+   * @example Example usage:
+   * valueToString(123);       // Outputs: '123'
+   * valueToString(3.14);      // Outputs: '3.14'
+   * valueToString([1, 2, 3]); // Outputs: '[1, 2, 3]'
+   */
+  private function valueToString($value) : string
+  {
+    return \is_array($value) ? \implode(',', $value) : (string) $value;
+  }
+
+  /**
+   * Creates a tupples
+   * 
+   * @param string|string[][]|array<string,string> $params [optional]
+   * @return array<string,array|string>
+   */
+  private function makeTupples($params) : array
+  {
+    $tupples = [];
+
+    // Handle: If params is query string (e.g., 'user=abcd&id=12345')
+    if (\is_string($params)) {
+      \parse_str($params, $tupples);
+      return $tupples;
+    }
+
+    if (\is_array($params) && !$this->isAssoc($params)) {
+      foreach($params as $i=> $tuple) {
+        if (!\is_array($tuple) ||
+          ($this->isAssoc($params) && \count($tuple) > 1) || (\count($tuple) > 2 || \count($tuple) <= 1)) {
+          $this->throwTypeError($i);
+        }
+      }
+    }
+
+    return (array) $params;
+  }
+
+  /**
+   * Creates a real entries.
+   * 
+   * @param string|string[][]|array<string,string> $params [optional]
+   * @return array<array<string,string>>
+   */
+  private function makeEntries($params) : array
+  {
+    $entries = [];
+    foreach($this->tupples() as $key => $value) {
+      $this->setRecursive((string)$key, $value, $entries);
+    }
+    return $entries;
+  }
+
+  /**
+   * Returns array param to query string.
+   * 
+   * @return string
+   */
+  public function toString() : string
+  {
+    $queries = [];
+    foreach($this->entries() as $entries) {
+      // $entries[0] for key representation, $entries[1] for val representation.
+      $queries[] = \sprintf('%s=%s', \urlencode($entries[0]), \urlencode($entries[1]));
+    }
+
+    return \join('&', $queries);
+  }
+
+  /**
+   * binding param with '[]' bracket paire
+   * 
+   * @param string $prefix   [required]
+   * @param mixed  $mixed    [required]
+   * @param array  &$entries [required]
+   * 
+   * @return void
+   */
+  private function setRecursive(string $prefix, $mixed, array &$entries) {
+    if (\is_array($mixed)) {
+      $isAssoc = $this->isAssoc($mixed);
+      foreach($mixed as $key => $value) {
+        $this->setRecursive(\sprintf('%s[%s]', $prefix, $isAssoc ? $key : ''), $value, $entries);
+      }
+    } else {
+      \array_push($entries, [$prefix, (string)$mixed]);
+    }
+  }
+
+  /**
+   * Creates a tupples
+   * 
+   * @return array<string,array|string>
+   */
+  public function tupples() : array
+  {
+    return $this->tupples ?? $this->makeTupples($this->params);
+  }
+
+  /**
+   * throwTypeError - method will throw TypeError,
+   * It's used to be if query paire not be iterable [name, value] tuple.
+   * 
+   * @param int $i [required]
+   * 
+   * @throws \TypeError Used to throw, if query paire not be iterable [name, value] tuple.
+   * @return void
+   */
+  private function throwTypeError(int $i) : void
+  {
+    throw new \TypeError(\sprintf(
+      '[ERR_INVALID_TUPLE]: Each query pair must be an iterable [name, value] tuple on index [%d]', $i)
+    );
   }
 }
 ?>
